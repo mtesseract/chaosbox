2072f791d527971042accd2743b3b65b445fd3ae
Moritz Clasmeier <mtesseract@silverratio.net>
2019-12-20 06:29:12 +0100

    Use standard coordinate system with the origin in the center

--- src/ChaosBox.hs
+++ src/ChaosBox.hs
@@ -1,36 +1,38 @@
 module ChaosBox
-  ( runChaosBoxWith
-  , runChaosBoxIO
-  , runChaosBoxIOWith
-  , fillScreenHSV
-  , Opts(..)
-  , module ChaosBox.Generate
-  , module ChaosBox.Color
-  ) where
+  ( runChaosBoxWith,
+    runChaosBoxIO,
+    runChaosBoxIOWith,
+    fillScreenHSV,
+    Opts (..),
+    module ChaosBox.Generate,
+    module ChaosBox.Color,
+  )
+where
 
-import           ChaosBox.Color
-import           ChaosBox.Generate
+import ChaosBox.Color
+import ChaosBox.Generate
+import Control.Monad.Random
+import Control.Monad.Reader
+import Data.IORef
+import Data.Maybe
+import Data.Semigroup ((<>))
+import Data.Time.Clock.POSIX
+import Graphics.Rendering.Cairo
+import Graphics.Rendering.Cairo.Matrix (Matrix (..))
+import Options.Applicative
+import System.Directory
 
-import           Control.Monad.Random
-import           Control.Monad.Reader
-import           Data.IORef
-import           Data.Maybe
-import           Data.Semigroup           ((<>))
-import           Data.Time.Clock.POSIX
-import           Graphics.Rendering.Cairo
-import           Options.Applicative
-import           System.Directory
-
-data Opts = Opts
-  { optSeed           :: Maybe Int
-  , optScale          :: Double
-  , optWidth          :: Int
-  , optHeight         :: Int
-  , optRenderTimes    :: Int
-  , optName           :: String
-  , optRenderProgress :: Bool
-  , optMetadataString :: Maybe String
-  }
+data Opts
+  = Opts
+      { optSeed :: Maybe Int,
+        optScale :: Double,
+        optWidth :: Int,
+        optHeight :: Int,
+        optRenderTimes :: Int,
+        optName :: String,
+        optRenderProgress :: Bool,
+        optMetadataString :: Maybe String
+      }
 
 opts :: Parser Opts
 opts =
@@ -38,81 +40,78 @@ opts =
     <$> optional (option auto $ long "seed" <> metavar "SEED")
     <*> option auto (long "scale" <> metavar "SCALE" <> value 1)
     <*> option auto (long "width" <> short 'w' <> metavar "WIDTH" <> value 100)
-    <*> option auto
-               (long "height" <> short 'h' <> metavar "HEIGHT" <> value 100)
+    <*> option
+      auto
+      (long "height" <> short 'h' <> metavar "HEIGHT" <> value 100)
     <*> option auto (long "times" <> metavar "TIMES" <> value 1)
     <*> strOption (long "name" <> metavar "NAME" <> value "sketch")
     <*> switch (long "render-progress" <> short 'r')
     <*> optional (strOption (long "metadata" <> metavar "METADATA"))
 
 optsInfo :: ParserInfo Opts
-optsInfo = info
-  (opts <**> helper)
-  (fullDesc <> progDesc "Generate art with ChaosBox" <> header "chaosbox")
+optsInfo =
+  info
+    (opts <**> helper)
+    (fullDesc <> progDesc "Generate art with ChaosBox" <> header "chaosbox")
 
-runChaosBoxIO
-  :: RandT StdGen (ReaderT GenerateCtx Render) a
-  -- ^ Render function
-  -> IO ()
+runChaosBoxIO ::
+  -- | Render function
+  RandT StdGen (ReaderT GenerateCtx Render) a ->
+  IO ()
 runChaosBoxIO render = do
   options <- execParser optsInfo
   runChaosBoxWith options render
 
-runChaosBoxIOWith
-  :: (Opts -> Opts)
-  -- ^ Option modifier
-  -> RandT StdGen (ReaderT GenerateCtx Render) a
-  -- ^ Render function
-  -> IO ()
+runChaosBoxIOWith ::
+  -- | Option modifier
+  (Opts -> Opts) ->
+  -- | Render function
+  RandT StdGen (ReaderT GenerateCtx Render) a ->
+  IO ()
 runChaosBoxIOWith fn render = do
   options <- execParser optsInfo
   runChaosBoxWith (fn options) render
 
-runChaosBoxWith
-  :: Opts
-  -- ^ Art options
-  -> RandT StdGen (ReaderT GenerateCtx Render) a
-  -- ^ Render function
-  -> IO ()
+runChaosBoxWith ::
+  -- | Art options
+  Opts ->
+  -- | Render function
+  RandT StdGen (ReaderT GenerateCtx Render) a ->
+  IO ()
 runChaosBoxWith Opts {..} doRender = replicateM_ optRenderTimes $ do
   seed <- case optSeed of
     Just seed' -> pure seed'
-    Nothing    -> round . (* 1000) <$> getPOSIXTime
-
+    Nothing -> round . (* 1000) <$> getPOSIXTime
   let stdGen = mkStdGen seed
-      w      = round $ fromIntegral optWidth * optScale
-      h      = round $ fromIntegral optHeight * optScale
-
-  surface     <- createImageSurface FormatARGB32 w h
+      w = round $ fromIntegral optWidth * optScale
+      h = round $ fromIntegral optHeight * optScale
+  surface <- createImageSurface FormatARGB32 w h
   progressRef <- newIORef 0
-
   -- Create directories if they don't exist
   createDirectoryIfMissing False $ "./images/"
   createDirectoryIfMissing False $ "./images/" <> optName
   createDirectoryIfMissing False $ "./images/" <> optName <> "/progress"
-
   beforeSaveHookRef <- newIORef Nothing
-
-  let ctx = GenerateCtx optWidth
-                        optHeight
-                        seed
-                        optScale
-                        optName
-                        optRenderProgress
-                        progressRef
-                        beforeSaveHookRef
-
+  let ctx =
+        GenerateCtx
+          optWidth
+          optHeight
+          seed
+          optScale
+          optName
+          optRenderProgress
+          progressRef
+          beforeSaveHookRef
   void . renderWith surface . flip runReaderT ctx . flip runRandT stdGen $ do
-    cairo $ scale optScale optScale
+    cairo $ do
+      scale optScale optScale
+      setMatrix (stdCoordinateSystem optWidth optHeight)
     void doRender
-
-    ref   <- asks gcBeforeSaveHook
+    ref <- asks gcBeforeSaveHook
     mHook <- liftIO $ readIORef ref
-
     case mHook of
-      Nothing   -> pure ()
+      Nothing -> pure ()
       Just hook -> hook
-
   putStrLn "Generating art..."
   let filename =
         "images/"
@@ -124,10 +123,8 @@ runChaosBoxWith Opts {..} doRender = replicateM_ optRenderTimes $ do
           <> fromMaybe "" optMetadataString
           <> ".png"
       latest = "images/" <> optName <> "/latest.png"
-
   putStrLn $ "Writing " <> filename
   putStrLn $ "Writing " <> latest
-
   surfaceWriteToPNG surface filename
   surfaceWriteToPNG surface latest
 
@@ -139,3 +136,7 @@ fillScreenHSV color = do
   cairo $ do
     rectangle 0 0 w h
     setSourceHSV color *> fill
+
+stdCoordinateSystem :: Int -> Int -> Matrix
+stdCoordinateSystem width height =
+  Matrix 1 0 0 (-1) (fromIntegral width / 2) (fromIntegral height / 2)
